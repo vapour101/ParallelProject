62bb093694e49ece6cc67014b1e1ec098a649cd9
Tau Merand <tau.merand@students.wits.ac.za>
2017-10-20 10:19:10 +0200

    More rough plots

--- TestData/data.py
+++ TestData/data.py
@@ -2,30 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-def join_data(filestring="data*", out='full_sorted'):
-    files=glob.glob(filestring)
-    matrix_list=[np.loadtxt(f, delimiter=',') for f in files]
 
-    raw_data=np.concatenate(matrix_list)
-    sorted = raw_data[raw_data[:, 3].argsort()]
+def join_data(filestring="data:*", out='full_sorted'):
+    files = glob.glob(filestring)
+    matrix_list = [np.loadtxt(f, delimiter=',') for f in files]
+
+    raw_data = np.concatenate(matrix_list)
     sorted = raw_data[raw_data[:, 3].argsort()]
 
     with open(out, 'wb') as f:
         np.savetxt(f, sorted, delimiter=',', fmt='%.5f')
 
+
 def cheat(infile='full_sorted'):
-    better=[]
-    a=np.loadtxt(infile, delimiter=',')
+    better = []
+    a = np.loadtxt(infile, delimiter=',')
     for row in a:
-        if row[0]>row[1]:
+        if row[0] > row[1]:
             better.append(row)
-    better_matrix=np.array(better)
+    better_matrix = np.array(better)
 
     with open('better_data', 'wb') as f:
         np.savetxt(f, better_matrix, delimiter=',', fmt='%.5f')
 
+join_data()
+
 cheat()
-
-
-
-
--- TestData/grapher.py
+++ TestData/grapher.py
@@ -2,7 +2,7 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_all(filestring="*data"):
+def plot_all(filestring="data:*"):
     files=glob.glob(filestring)
     matrix_list=[np.loadtxt(f, delimiter=',') for f in files]
     for i in range(1, len(matrix_list)+1):
@@ -15,8 +15,8 @@ def plot_all(filestring="*data"):
 
 def plot_better():
     a=np.loadtxt("better_data", delimiter=',')
-    plt.semilogy(a[0:100, 0], 'ro')
-    plt.semilogy(a[0:100, 1], 'b+')
+    plt.semilogy(a[0:200, 0], 'ro')
+    plt.semilogy(a[0:200, 1], 'b+')
     plt.show()
     #plt.savefig("better_graph")
 
