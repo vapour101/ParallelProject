b41cd07242aa8ed9198da62468d50e452c421bf0
Vapour101 <vapour101@gmail.com>
2017-10-28 13:40:49 +0200

    New testdata

--- ParallelBacktrack/main.cpp
+++ ParallelBacktrack/main.cpp
@@ -12,8 +12,7 @@ int stringToInt(const string& str);
 
 int main(int argc, char* argv[])
 {
-	/*
-	int averageOver = 1;
+	int averageOver = 10;
 
 	if (argc < 2)
 		return 0;
@@ -23,7 +22,7 @@ int main(int argc, char* argv[])
 		averageOver = stringToInt(argv[2]);
 	}
 
-	cout << "sequential time, parallel time, solution length, nodes touched, infeasible states counted" << endl;
+	cout << "time (ns), solution length, nodes touched, infeasible states counted" << endl;
 
 	string fileName = argv[1];
 	vector<string> boards = readLinesFromFile(fileName);
@@ -31,49 +30,22 @@ int main(int argc, char* argv[])
 	for (string board : boards)
 	{
 		Board check{board};
-		backtrack checker{check};
-		checker.setParallel(true);
-		checker.start();
+		BackTrack solver{check};
 
-		checker.print(cout);
-
-		if (checker.hasSolution())
-			checker.printSequence(cout);
-
-		cout << "*************" << endl;
-
-		chrono::nanoseconds sequentialTime = chrono::nanoseconds::zero();
-		chrono::nanoseconds parallelTime = chrono::nanoseconds::zero();
+		chrono::nanoseconds time = chrono::nanoseconds::zero();
 
 		for (int i = 0; i < averageOver; ++i)
 		{
-			checker.clear();
-			checker.start();
-			sequentialTime += checker.getDuration();
+			solver.clear();
+			solver.start();
+			time += solver.getDuration();
 		}
 
-		checker.setParallel(true);
+		time /= averageOver;
 
-		for (int i = 0; i < averageOver; ++i)
-		{
-			checker.clear();
-			checker.start();
-			parallelTime += checker.getDuration();
-		}
-
-		sequentialTime /= averageOver;
-		parallelTime /= averageOver;
-
-		cout << sequentialTime.count() << ", " << parallelTime.count() << ", ";
-		cout << checker.getSolution().size() << ", " << checker.getNodes() << ", ";
-		cout << checker.getInfeasibleCount() << endl;
-	}*/
-
-	Board board;
-	BackTrack solver{board};
-
-	solver.start();
-	cout << solver << solver.getBoardSequence();
+		cout << time.count() << ", " << solver.getMoveSequence().size() << ", ";
+		cout << solver.getNodes() << ", " << solver.getInfeasibleCount() << endl;
+	}
 
 	return 0;
 }
